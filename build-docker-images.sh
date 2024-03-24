6c23b9c4af1860971bac13e68ac9902cdbb9e665
Ümit <ciftciumit42@gmail.com>
2024-03-24 22:11:12 +0300

    Dockerfiles add

---
+++ build-docker-images.sh
@@ -0,0 +1,3 @@
+docker build --force-rm -t "backend-server:tag" ./backend-server
+docker build --force-rm -t "frontend-server:tag" ./frontend-server
+docker build --force-rm -t "database-server:tag" ./database-server
---
+++ push-docker-images.sh
@@ -0,0 +1,3 @@
+docker push umitciftci/backend-server:tag
+docker push umitciftci/frontend-server:tag
+docker push umitciftci/database-server:tag
